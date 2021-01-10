49b64bd6fbd739004422eb31eea41b21b9c880e8
José Meurer <josewm18@gmail.com>
2021-01-10 09:43:28 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,16 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import Routers from './Routers';
 
 function App() {
   return (
-    <Routers />
-    );
+    <>
+      <Routers />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
