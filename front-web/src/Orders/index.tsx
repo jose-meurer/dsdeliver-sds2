2e133ec46fbe63b154b89be44f1a0500f9934d03
José Meurer <josewm18@gmail.com>
2021-01-10 09:54:14 -0300

    Finish front-web

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
+import { toast} from 'react-toastify';
 import { fetchProducts, saveOrder } from '../api';
 import Footer from '../Footer';
 import { checkIsSelected } from './helpers';
